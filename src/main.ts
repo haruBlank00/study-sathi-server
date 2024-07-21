1a96d0e016fc03c674adecd5ee9fb8dda452fb50
anup kafle <anup.kafle@varosatech.com>
2024-07-21 09:15:45 +0545

    revert changes

--- src/main.ts
+++ src/main.ts
@@ -42,6 +42,6 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(8000);
+  await app.listen(5000);
 }
 bootstrap();
